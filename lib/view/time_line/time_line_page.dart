2fb1ce4f71f4f20fbea52ca0086bc617fe5aaf7d
岡本紗也加 <impl@implnoMacBook-Pro-14.local>
2023-04-14 00:11:20 +0900

    投稿ページのUI part1

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 
 import '../../model/account.dart';
 import '../../model/post.dart';
+import 'post_page.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -89,6 +90,12 @@ class _TimeLinePageState extends State<TimeLinePage> {
             ),
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const PostPage()));
+        },
+        child: const Icon(Icons.chat_bubble_outline)//右下に吹き出しアイコンの丸いボタンが作成される
       ),
     );
   }
